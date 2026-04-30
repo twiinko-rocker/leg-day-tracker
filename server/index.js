3014238c6041b1a1dfcc5c389c78037498585910
Alan Correa <am.correagarcia@gmail.com>
2026-04-30 17:28:09 -0600

    feat: connect mogoose to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 const app = express();
+app
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
